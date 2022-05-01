704ad04d01805f9e29b46c7cc247de4162f6d0ec
lucas5203 <lucasdanielbrandan0@gmail.com>
2022-05-01 15:24:34 -0300

    agg saludo

--- prueba1.c
+++ prueba1.c
@@ -1,12 +1,19 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 int sumar(int, int);
 int restar(int, int);
 int cociente(int, int);
 void mostrar(int);
+void mensaje(char *);
+
 int main(){
 
     int a, b;
+    char *nombre = (char *) malloc(sizeof(char) * 50);
+    printf("Ingrese su nombre: ");
+    gets(nombre);
+    mensaje(nombre);
 
     printf("Ingrese 'a': ");
     scanf("%d", &a);
@@ -39,4 +46,8 @@ int cociente(int x, int y){
 
 void mostrar(int resultado){
     printf("\nEl valor es %d\n", resultado);
+}
+
+void mensaje(char *nombre){
+    printf("Hola %s!!\n", *nombre);
 }
